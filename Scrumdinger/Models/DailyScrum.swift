412c91b92cf89295e65ebafb9e897a3bb59800db
Cris Messias <meunomeecriss@gmail.com>
2023-09-30 17:35:57 +0200

    :construction: Finishing the Scrum List
    The scrum list display the data fot each scrum

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    var id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 
@@ -21,11 +30,11 @@ extension DailyScrum {
                    attendees: ["Katy",  "Vincent", "Cris", "Bea"],
                    lengthInMinutes: 10,
                    theme: .yellow),
-        DailyScrum(title: "Cours",
+        DailyScrum(title: "Cours de Français",
                    attendees: ["Marcus",  "Durontina", "Chris", "Agustina"],
                    lengthInMinutes: 15,
                    theme: .orange),
-        DailyScrum(title: "Café internacional",
+        DailyScrum(title: "Café Internacional",
                    attendees: ["Coline",  "Bruno", "Marie", "Anuski"],
                    lengthInMinutes: 5,
                    theme: .poppy)
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Cris Messias on 30/09/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
