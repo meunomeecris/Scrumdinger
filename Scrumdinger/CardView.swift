eac6d11186940574959a05e5235882f98645dc80
Cris Messias <meunomeecriss@gmail.com>
2023-10-01 12:22:41 +0200

    :bug: Fixing the preview
    Using the newest way to display a preview in SwiftUI

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -30,11 +30,9 @@ struct CardView: View {
     }
 }
 
-struct CardView_Previews: PreviewProvider {
-    static var scrum = DailyScrum.sampleData[0]
-    static var previews: some View {
-        CardView(scrum: scrum)
-            .background(scrum.theme.mainColor)
-            .previewLayout(.fixed(width: 400, height: 60))
-    }
+#Preview {
+    CardView(scrum: DailyScrum.sampleData[0])
+        .background(DailyScrum.sampleData[0].theme.mainColor)
+        .previewLayout(.fixed(width: 400, height: 60))
 }
+
